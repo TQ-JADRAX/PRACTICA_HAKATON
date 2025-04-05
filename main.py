8fb773c9f96c8645a32becd2c0a6e14e7a3d2e0f
¨Alexys¨ <example@gmail.com>
2025-04-05 10:55:56 -0600

    ¨Clase_Principa_realizada_ALEXYS¨

---
+++ main.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+class CUENTA_BANCARIA:
+    def __init__(self, titular, saldo_inicial):
+        self._titular = titular
+        self._saldo = saldo_inicial
+
+    def depositar(self, cantidad):
+        self._saldo += cantidad
+
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @saldo.setter    
+    def saldo(self, saldo):
+        self._saldo = saldo
+    
+    @property
+    def titular(self):
+        return self._titular
+    
+    @titular.setter
+    def titular(self, titular):
+        self._titular = titular
+ 
+    @abstractmethod
+    def info(self, cantidad):
+        pass
+    
+    
+    
+        
+        
+        
+        
