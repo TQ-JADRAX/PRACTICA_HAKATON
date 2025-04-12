67054e0e3016c110561bbc33bb854ab96d4af87b
¨Alexys¨ <example@gmail.com>
2025-04-12 11:05:43 -0600

    TKT-Alex

--- objetos.py
+++ objetos.py
@@ -11,6 +11,7 @@ cuenta1 = Cuenta_Inicial('Luis',2000)
 cuenta2 = Cuenta_Inicial('Alexys',2000)
 cuenta3 = Cuenta_Inicial('Migue',2000)
 cuenta1.info()
-
-
+cuenta1.Depositar()
+cuenta1.info()
+cuenta1.Retirar()
 
---
+++ tkt_devop/tkt_teter_003.md
@@ -0,0 +1,12 @@
+# NEW
+## Adicion funciones:
+    INFO
+## EXTRA
+    nueva clase, heredada de principal
+    nuevos objetos
+# PETICION
+##   testear funciones: 
+    info
+    depositar
+    retirar
+
